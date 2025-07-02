7e982610a62b0f38aa8bdd6f21b7939d44eb390b
unknown <udroiu.ion.gabriel@gmail.com>
2025-07-02 12:57:52 -0700

    day 3

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,5 +1,3 @@
 <div>
-    <h2>Salut Laravel!</h2>
-    <h2>Salut Laravel!</h2>
-    <h2>Salut Laravel!2</h2>
+    {{-- Do your work, then step back. --}}
 </div>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Livewire</title>
+    @livewireStyles
+</head>
+<body>
+    <h1>Hello Livewire Test</h1>
+    @livewireScripts
+</body>
+</html>
